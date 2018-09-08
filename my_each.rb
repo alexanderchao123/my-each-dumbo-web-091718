e37aa9ee5aeded33639228a3a967f80903b6ef3b
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-08 03:47:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  return "This block should not run!" if array.empty?
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  return array
 end
